e82281b64d7cd65c58313b5f2d4769764932e3be
Eric <mcr.988899@163.com>
2021-03-03 20:58:54 +0800

    菜单和标签页

--- src/views/homePage/menuConfig.js
+++ src/views/homePage/menuConfig.js
@@ -2,6 +2,13 @@
 //type:1-文件夹, 2-链接
 export const menuList = [
     {
+        title: '管理员管理',
+        url: '/#/testPage2',
+        type: 2,
+        icon: 'windows',
+        subMenuList: []
+    },
+    {
         title: '客户管理',
         type: 1,
         icon: 'pie-chart',
