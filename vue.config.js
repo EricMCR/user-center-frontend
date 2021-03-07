eee5188bd3f7126e69f95e57d76732b3f7355e66
Eric Ma <mcr.988899@163.com>
2021-03-07 17:02:00 +0800

    save

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,10 @@
 module.exports = {
     devServer: {
+        port: 8888,
         proxy: {
             '/api': {
                 //target要改为服务所在的服务器IP
-                target: 'http://192.168.0.107:8080',
+                target: 'http://127.0.0.1:8082',
                 // 允许跨域
                 changeOrigin: true,
                 ws: true,
